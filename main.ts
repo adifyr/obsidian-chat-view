892ae3b2dba367ea86cace931e7b89c01095dfbd
Aditya Majethia <aditya@synerflow.in>
2022-03-26 22:03:46 +0530

    Cleaned up code. Refined styles. Ready to publish.

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ const COLORS = [ "red", "yellow", "blue", "purple", "green", "grey" ];
 export default class ChatViewPlugin extends Plugin {
 
 	override async onload(): Promise<void> {
-		this.registerMarkdownCodeBlockProcessor("chat", (source, element, ctx) => {
+		this.registerMarkdownCodeBlockProcessor("chat", (source, el, _) => {
 			const rawLines = source.split("\n").filter((line) => /(^>|<|\^)|...|\[(.*?)\]|^#/.test(line.trim()));
 			const lines = rawLines.map((rawLine) => rawLine.trim());
 			const colorConfigs = new Map<string, string>();
@@ -22,47 +22,33 @@ export default class ChatViewPlugin extends Plugin {
 			for (let index = 0; index < lines.length; index++) {
 				const line = lines[ index ].trim();
 				if (/^#/.test(line)) {
-					element.createEl("p", {text: line.substring(1).trim(), cls: [ "chat-view-comment" ]})
+					el.createEl("p", {text: line.substring(1).trim(), cls: [ "chat-view-comment" ]})
 				} else if (line === "...") {
-					this.createDelimiter(element);
+					const delimiter = el.createDiv({cls: [ "delimiter" ]});
+					for (let i = 0; i < 3; i++) delimiter.createDiv({cls: [ "dot" ]});
 				} else if (/^>|<|\^/.test(line)) {
 					const components = line.substring(1).split("|");
 					if (components.length > 0) {
-						const header = components.length > 1 && /[a-zA-Z0-9]/.test(components[ 0 ])
-							? components[ 0 ].trim() : "";
-						const message = components.length > 1 ? components[ 1 ].trim() : components[ 0 ].trim();
-						const subtext = components.length > 2 ? components[ 2 ].trim() : "";
-						const continued = index > 0 && line.charAt(0) === lines[ index - 1 ].charAt(0)
-							&& header.length === 0;
-						this.createChatBubble(
-							header, message, subtext, KEYMAP[ line.charAt(0) ], element, continued, colorConfigs
-						);
+						const first = components[ 0 ];
+						const head = components.length > 1 && /[a-zA-Z0-9]/.test(first) ? first.trim() : "";
+						const msg = components.length > 1 ? components[ 1 ].trim() : first.trim();
+						const sub = components.length > 2 ? components[ 2 ].trim() : "";
+						const continued = index > 0 && line.charAt(0) === lines[ index - 1 ].charAt(0) && head === "";
+						this.createChatBubble(head, msg, sub, KEYMAP[ line.charAt(0) ], el, continued, colorConfigs);
 					}
 				}
 			}
 		});
-	}
-	createDelimiter(element: HTMLElement) {
-		const delimiter = element.createDiv({cls: [ "delimiter" ]});
-		for (let i = 0; i < 3; i++) {
-			delimiter.createDiv({cls: [ "dot" ]});
-		}
 	}
 
 	createChatBubble(
 		header: string, message: string, subtext: string, align: string, element: HTMLElement, continued: boolean,
 		colorConfigs: Map<string, string>
 	) {
-		const bubble = element.createDiv({
-			cls: [
-				"chat-view-bubble",
-				`chat-view-align-${ align }`,
-				continued ? "chat-view-small-vertical-margin" : "chat-view-default-vertical-margin"
-			],
-		});
-		if (header.length > 0) bubble.createEl("h4", {
-			text: header, cls: [ "chat-view-header", `chat-view-${ colorConfigs.get(header) }` ]
-		});
+		const marginClass = continued ? "chat-view-small-vertical-margin" : "chat-view-default-vertical-margin";
+		const colorConfigClass = `chat-view-${ colorConfigs.get(header) }`;
+		const bubble = element.createDiv({cls: [ "chat-view-bubble", `chat-view-align-${ align }`, marginClass ]});
+		if (header.length > 0) bubble.createEl("h4", {text: header, cls: [ "chat-view-header", colorConfigClass ]});
 		if (message.length > 0) bubble.createEl("p", {text: message, cls: [ "chat-view-message" ]});
 		if (subtext.length > 0) bubble.createEl("sub", {text: subtext, cls: [ "chat-view-subtext" ], });
 	}
