d9a81b46b791024046e2b780da8befc2c4bf7354
Aditya Majethia <aditya@synerflow.in>
2026-05-25 14:28:59 +0530

    Cleaned up code and fixed few tsconfig issues.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { Plugin } from "obsidian";
+import {Plugin} from "obsidian";
 
 export default class ChatViewPlugin extends Plugin {
 	async onload() {
+		// Configure resources needed by the plugin.
+		console.log("Loading Plugin...")
 	}
 
 	onunload() {
+		// Release any resources needed by the plugin.
+		console.log("Unloading Plugin...")
 	}
 }
